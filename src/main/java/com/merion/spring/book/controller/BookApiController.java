a299032f4671afe8e639440736cb5da124bf6c5b
A.Lapshin <polick@mail.ru>
2024-01-23 19:38:43 +0200

    Commit with Add restController

--- src/main/java/com/merion/spring/book/controller/BookApiController.java
+++ src/main/java/com/merion/spring/book/controller/BookApiController.java
@@ -1,2 +1,12 @@
-package com.merion.spring.book.controller;public class BookApiController {
+package com.merion.spring.book.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookApiController {
+    @GetMapping("/")
+    public String ok() {
+        return "ok";
+    }
 }
