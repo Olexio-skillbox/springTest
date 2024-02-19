3753924c182595aea8f6e8f5dc0592c1cf644a14
A.Lapshin <polick@mail.ru>
2024-02-19 20:17:39 +0200

    Add CREATE request

--- src/main/java/com/merion/spring/book/controller/BookApiController.java
+++ src/main/java/com/merion/spring/book/controller/BookApiController.java
@@ -1,12 +1,39 @@
 package com.merion.spring.book.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.merion.spring.base.exeption.ResourceNotFoundExeption;
+import com.merion.spring.book.entity.BookEntity;
+import com.merion.spring.book.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BookApiController {
+    public final BookService bookService;
+
+    public BookApiController(BookService bookService) {
+        this.bookService = bookService;
+    }
     @GetMapping("/")
-    public String ok() {
+/*    public String ok() {
         return "ok";
+    }*/
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/api/v1/book")
+    public List<BookEntity> all() {
+        return bookService.all();
+    }
+
+    @GetMapping("/api/v1/book/{id}")
+    public BookEntity byId(@PathVariable Integer id) {
+        //return bookService.byId(id).orElseThrow(RuntimeException::new);
+        return bookService.byId(id).orElseThrow(ResourceNotFoundExeption::new);
+    }
+@PostMapping("/api/v1/book")
+    public BookEntity create(@RequestBody BookEntity request) {
+        return bookService.create(request.getTitle(), request.getDescription());
     }
 }
---
+++ src/main/java/com/merion/spring/book/service/BookService.java
@@ -0,0 +1,44 @@
+package com.merion.spring.book.service;
+
+import com.merion.spring.book.entity.BookEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class BookService {
+    static List<BookEntity> bookStorage = new ArrayList<>();
+
+    public BookService() {
+        fillStorage();
+    }
+    public void fillStorage() {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            BookEntity book = new BookEntity();
+            book.setId(i);
+            book.setTitle("Book № " + random.nextInt(100, 999));
+            book.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+            bookStorage.add(book);
+        }
+    }
+
+    public List<BookEntity> all() {
+        return bookStorage;
+    }
+    public Optional<BookEntity> byId(Integer id) {
+        return bookStorage.stream().filter((book -> book.getId().equals(id))).findFirst();
+    }
+
+    public BookEntity create(String title, String description) {
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setId(bookStorage.size());
+        bookEntity.setTitle(title);
+        bookEntity.setDescription(description);
+        bookStorage.add(bookEntity);
+        return bookEntity;
+    }
+}
